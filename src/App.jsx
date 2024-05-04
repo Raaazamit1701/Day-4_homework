85792dbb15a185c5b262ddf152f4827b0849dc7d
amit raj <amitraaaz0@gmial.com>
2024-05-04 20:32:17 +0530

    commpleted

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './component/Header'
+import Work from './component/Work'
+import Skills from './component/Skills'
+import Footer from './component/Footer'
+
+function App() {
+  
+  return (
+    <>
+       <Header />
+       <Work />
+       <Skills />
+       <Footer />
+    </>
+  )
+}
+
+export default App
